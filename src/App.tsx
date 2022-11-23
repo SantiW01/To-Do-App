3ffe75a4ddf2d71d54a8743c5705e447a7942e7b
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-11-23 20:47:37 -0300

    Adding ToDo's interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import InputField from "./Components/InputField";
+import ToDo from "./Model";
 import "./Style/App.css";
 
 function App(): JSX.Element {
   const [todo, setToDo] = useState<string>("");
+  const [arrayToDos, setTArrayToDos] = useState<ToDo[]>([]);
   console.log(todo);
   return (
     <div className="App">
---
+++ src/Model.ts
@@ -0,0 +1,5 @@
+export default interface ToDo {
+  id: number;
+  ToDo: string;
+  isDone: boolean;
+}
