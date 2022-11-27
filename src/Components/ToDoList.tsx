a6fe295ecf3ce61b13d998d570d86083eb4f6679
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-11-27 00:42:24 -0300

    Add Completed Task's section

--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -10,10 +10,29 @@ interface Props {
 
 export default function ToDoList({ toDo, setToDo }: Props): JSX.Element {
   return (
-    <div className="todos">
-      {toDo.map((todo) => (
-        <SingleToDo toDo={todo} key={todo.id} toDos={toDo} setToDos={setToDo} />
-      ))}
+    <div className="container">
+      <div className="toDos">
+        <span className="toDos__heading">Active task</span>
+        {toDo.map((todo) => (
+          <SingleToDo
+            toDo={todo}
+            toDos={toDo}
+            setToDos={setToDo}
+            key={todo.id}
+          />
+        ))}
+      </div>
+      <div className="toDos remove">
+        <span className="toDos__heading">Complete task</span>
+        {toDo.map((todo) => (
+          <SingleToDo
+            toDo={todo}
+            toDos={toDo}
+            setToDos={setToDo}
+            key={todo.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
